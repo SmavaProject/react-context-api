309ae0acfc339d7242bf0c16f84a405d97dde5af
SmavaProject <katyakravchenka@yahoo.com>
2020-05-31 19:29:23 -0700

    refactoring components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Provider} from "./components/Context";
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
-  <App />, 
+    <Provider>
+        <App/>
+    </Provider>,
   document.getElementById('root')
 );
